6d92507f794a373e223d067af3e910cc197d825a
Rishi Srivastav <rishiaiweb@gmail.com>
2026-08-03 19:39:16 +0530

    Time: 0 ms (100%), Space: 12.5 MB (47.92%) - LeetHub

---
+++ 0061-rotate-list/0061-rotate-list.py
@@ -0,0 +1,39 @@
+# Definition for singly-linked list.
+class ListNode(object):
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+class Solution(object):
+    def rotateRight(self, head, k):
+        if not head or not head.next or k == 0:
+            return head
+
+        # Find length and last node
+        length = 1
+        tail = head
+        while tail.next:
+            tail = tail.next
+            length += 1
+
+        # Reduce k
+        k = k % length
+        if k == 0:
+            return head
+
+        # Make circular
+        tail.next = head
+
+        # Find new tail
+        steps = length - k - 1
+        new_tail = head
+        for _ in range(steps):
+            new_tail = new_tail.next
+
+        # New head
+        new_head = new_tail.next
+
+        # Break circle
+        new_tail.next = None
+
+        return new_head
